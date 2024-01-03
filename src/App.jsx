c6d6e88fe3cebec49b9bdf4ad9c2e527a7913ece
fabiodplacerda <lacerda96@gmail.com>
2024-01-03 10:05:37 +0000

    end turn functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import { TableCards } from "./components/TableCards";
 import { CardPile } from "./components/CardPile";
 import Header from "./components/Header";
 import GameOver from "./components/GameOver";
+import EndTurn from "./components/EndTurn";
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [hasStarted, setHasStarted] = useState(false);
   const [hasSetup, setHasSetup] = useState(false);
-  const [handCards, setHandCards] = useState([])
-  const [score, setScore] = useState({player1:0, player2:0})
-  const [gameOver, setGameOver] = useState(false)
-
+  const [handCards, setHandCards] = useState([]);
+  const [score, setScore] = useState({ player1: 0, player2: 0 });
+  const [gameOver, setGameOver] = useState(false);
+  const [turnEnded, setTurnEnded] = useState(false);
   useEffect(() => {
     const onDisconnect = () => {
       setIsConnected(false);
@@ -29,34 +30,34 @@ function App() {
       setHasSetup(true);
     };
 
-    const onCardAdd = (drawnCard)=>{
-      setHandCards((previous)=>{
-        return [...previous, drawnCard]
-      })
-    }
+    const onCardAdd = (drawnCard) => {
+      setHandCards((previous) => {
+        return [...previous, drawnCard];
+      });
+    };
 
-    const onCardSell = (cardSoldId)=>{
-      setHandCards((previous)=>{
-        return [...previous].filter((card)=>card.id !== cardSoldId)
-      })
-    }
+    const onCardSell = (cardSoldId) => {
+      setHandCards((previous) => {
+        return [...previous].filter((card) => card.id !== cardSoldId);
+      });
+    };
 
-    const onResourceUpdate = (resources)=>{
-      setScore(resources)
-    }
+    const onResourceUpdate = (resources) => {
+      setScore(resources);
+    };
 
-    const onGameOver = ()=>{
-      setGameOver(true)
-      setHasStarted(false)
-    }
+    const onGameOver = () => {
+      setGameOver(true);
+      setHasStarted(false);
+    };
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
-    socket.on("game-setup", onGameSetup); // to connect with setup-game emitter from the server
+    socket.on("gameSetup", onGameSetup); // to connect with setup-game emitter from the server
     socket.on("cardAdded", onCardAdd);
-    socket.on("cardSold", onCardSell) 
-    socket.on("resourcesUpdated", onResourceUpdate)
-    socket.on("gameOver", onGameOver)
+    socket.on("cardSold", onCardSell);
+    socket.on("resourcesUpdated", onResourceUpdate);
+    socket.on("gameOver", onGameOver);
 
     return () => {
       socket.off("connect", onConnect);
@@ -64,24 +65,26 @@ function App() {
     };
   }, []);
 
-  console.log(isConnected);
-  console.log(socket);
-  console.log(hasStarted);
+  // console.log(isConnected);
+  // console.log(socket);
+  // console.log(hasStarted);
+  // console.log(turnEnded, "turnEnd Console.log");
 
   return (
     <>
       <h1>Card Game</h1>
-      <Header score = {score}/>
+      <Header score={score} />
       <GameStart hasStarted={hasStarted} setHasStarted={setHasStarted} />
       {hasStarted ? <h1>Game Started!</h1> : null}
       {hasSetup ? (
         <>
-          <HandCards handCards={handCards}/>
+          <HandCards handCards={handCards} />
           <TableCards />
           <CardPile />
+          <EndTurn turnEnded={turnEnded} setTurnEnded={setTurnEnded} />
         </>
       ) : null}
-      {gameOver ? <GameOver/> : null}
+      {gameOver ? <GameOver /> : null}
     </>
   );
 }
