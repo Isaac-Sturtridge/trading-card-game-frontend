fa9a7f4bb10ea231af2eb49107012fafb2699bef
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-01-05 10:18:53 +0000

    setting payload logic

--- src/components/EndTurn.jsx
+++ src/components/EndTurn.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { socket } from "../socket";
 
 const EndTurn = ({ turnEnded, setTurnEnded, selectedTableCards, selectedHandCards }) => {
-  const [action, setAction] = useState('')
+  const [action, setAction] = useState('Your turn')
+  const [payload, setPayload] = useState([])
 
-  // if(selectedTableCards.length === 1 && selectedHandCards.length === 0) {
-  //   setAction('addCardToHand')
-  // } else if(selectedHandCards.length > 0 && selectedTableCards.length === 0) {
-  //   setAction('sellCardFromHand')
-  // } else if(selectedHandCards.length === selectedTableCards.length && selectedHandCards.length > 0 && selectedTableCards.length > 0) {
-  //   setAction('cardSwap')
-  // } else {
-  //   setAction('')
-  // }
+  useEffect(() => {
+    if(selectedHandCards.length === 0 && selectedTableCards.length === 0) {
+      setAction('Your turn')
+    } else if(selectedTableCards.length === 1 && selectedHandCards.length === 0) {
+      setAction('addCardToHand')
+      setPayload({cards: selectedTableCards})
+    } else if(selectedHandCards.length > 0 && selectedTableCards.length === 0) {
+      setAction('sellCardFromHand')
+      setPayload({cards: selectedHandCards})
+    } else if(selectedHandCards.length === selectedTableCards.length) {
+      setAction('cardSwap')
+      setPayload({handCards: selectedHandCards, tableCards: selectedTableCards})
+    } else {
+      setAction('Invalid Move')
+    }
+  }, [selectedHandCards, selectedTableCards])
 
   const handleClick = () => {
-    socket.emit(action, )
+    socket.emit(action, payload)
     socket.emit("endTurn", turnEnded, () => {});
 
   }
@@ -25,7 +33,7 @@ const EndTurn = ({ turnEnded, setTurnEnded, selectedTableCards, selectedHandCard
       className="endTurnButton"
       disabled={!turnEnded}
       onClick={handleClick}
-    >{turnEnded ? "End Turn!" : "Opponents Turn!"}
+    >{turnEnded ? `${action}`: "Opponents Turn!"}
     </button>
   );
 };
