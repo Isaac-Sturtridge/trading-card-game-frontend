74e06122f0d02e88721d7a209ff7bfc02c127f35
fabiodplacerda <lacerda96@gmail.com>
2024-01-09 09:44:50 +0000

    limit card functionality

--- src/components/EndTurn.jsx
+++ src/components/EndTurn.jsx
@@ -1,47 +1,75 @@
 import { useEffect, useState } from "react";
 import { socket } from "../socket";
 
-const EndTurn = ({ turnEnded, setTurnEnded, selectedTableCards, selectedHandCards }) => {
-  const [action, setAction] = useState('Your turn')
-  const [payload, setPayload] = useState([])
+const EndTurn = ({
+  turnEnded,
+  setTurnEnded,
+  selectedTableCards,
+  selectedHandCards,
+  handCards,
+}) => {
+  const [action, setAction] = useState("Your turn");
+  const [payload, setPayload] = useState([]);
   const [actionLookup, setActionLookup] = useState({
-    'addCardToHand': 'Take card',
-    'sellCardFromHand': 'Sell card',
-    'cardSwap': 'Swap cards',
-    'Invalid Move': 'Invalid Move',
-    'Your turn': 'Your turn'
-  })
+    addCardToHand: "Take card",
+    sellCardFromHand: "Sell card",
+    cardSwap: "Swap cards",
+    "Invalid Move": "Invalid Move",
+    "Your turn": "Your turn",
+    maxCards: "Max card reached",
+  });
+
+  console.log(handCards.length);
 
   useEffect(() => {
-    if(selectedHandCards.length === 0 && selectedTableCards.length === 0) {
-      setAction('Your turn')
-    } else if(selectedTableCards.length === 1 && selectedHandCards.length === 0) {
-      setAction('addCardToHand')
-      setPayload({cards: selectedTableCards})
-    } else if(selectedHandCards.length > 0 && selectedTableCards.length === 0 && selectedHandCards.every((card) => card.card_type === selectedHandCards[0].card_type)) {
-      setAction('sellCardFromHand')
-      setPayload({cards: selectedHandCards})
-
-    } else if(selectedHandCards.length === selectedTableCards.length) {
-      setAction('cardSwap')
-      setPayload({handCards: selectedHandCards, tableCards: selectedTableCards})
+    if (selectedHandCards.length === 0 && selectedTableCards.length === 0) {
+      setAction("Your turn");
+    } else if (
+      selectedTableCards.length === 1 &&
+      selectedHandCards.length === 0 &&
+      handCards.length < 7
+    ) {
+      setAction("addCardToHand");
+      setPayload({ cards: selectedTableCards });
+    } else if (
+      selectedHandCards.length > 0 &&
+      selectedTableCards.length === 0 &&
+      selectedHandCards.every(
+        (card) => card.card_type === selectedHandCards[0].card_type
+      )
+    ) {
+      setAction("sellCardFromHand");
+      setPayload({ cards: selectedHandCards });
+    } else if (selectedHandCards.length === selectedTableCards.length) {
+      setAction("cardSwap");
+      setPayload({
+        handCards: selectedHandCards,
+        tableCards: selectedTableCards,
+      });
+    } else if (handCards.length >= 7) {
+      setAction("maxCards");
     } else {
-      setAction('Invalid Move')
+      setAction("Invalid Move");
     }
-  }, [selectedHandCards, selectedTableCards])
+  }, [selectedHandCards, selectedTableCards]);
 
   const handleClick = () => {
-    socket.emit(action, payload)
+    socket.emit(action, payload);
     socket.emit("endTurn", turnEnded, () => {});
-
-  }
+  };
 
   return (
     <button
       className={`endTurnButton ${action}`}
-      disabled={!turnEnded || action==="Invalid Move" || action==="Your turn"}
+      disabled={
+        !turnEnded ||
+        action === "Invalid Move" ||
+        action === "Your turn" ||
+        action === "maxCards"
+      }
       onClick={handleClick}
-    >{turnEnded ? `${actionLookup[action]}`: "Opponents Turn!"}
+    >
+      {turnEnded ? `${actionLookup[action]}` : "Opponents Turn!"}
     </button>
   );
 };
