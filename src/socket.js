4d122c4173c3f2055c0a0e1ce0dbee008697ad2d
Isaac-Sturtridge <eyes330@yahoo.co.uk>
2024-01-12 12:42:42 +0000

    prepare for build

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://resource-rivals.onrender.com' : 'http://localhost:3000';
 
 export const socket = io(URL, {
     autoConnect: false
